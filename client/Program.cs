de8a0b7e3e0ca5f5e34ef4bf281f35c5f0daa8cf
antbrr <antbr@itu.dk>
2023-03-22 16:05:16 +0100

    frontend progress

--- client/Program.cs
+++ client/Program.cs
@@ -1,11 +1,10 @@
-using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Web;
 using client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 await builder.Build().RunAsync();
--- test/Seeding.Tests.cs
+++ test/Seeding.Tests.cs
@@ -1,5 +1,5 @@
 namespace test;
-using server.Core.Services;
+using server.Services;
 
 public class SeedingTests
 {
