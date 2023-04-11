418b6f6b51412576d3ede96cd99ce179c0acf6ee
antbrr <antbr@itu.dk>
2023-04-11 18:27:30 +0200

    limit description characters + load categories without page refresh

--- server/Migrations/20230411152643_InitialCreate.Designer.cs
+++ server/Migrations/20230411152643_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using server.Infrastructure;
 namespace server.Migrations
 {
     [DbContext(typeof(NourishNexusContext))]
-    [Migration("20230409104915_InitialCreate")]
+    [Migration("20230411152643_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
