bbabf66f96ea64e9b87164a9db31914158462716
antbrr <antbr@itu.dk>
2023-05-01 10:25:02 +0200

    fixed error message on pages with "to top" button when scrolling

--- client/wwwroot/js/scroll.js
+++ client/wwwroot/js/scroll.js
@@ -1,12 +1,17 @@
-window.addEventListener('scroll', toggleScrollToTopButton);
+
 
 function toggleScrollToTopButton() {
+    window.addEventListener('scroll', toggleScrollToTopButton);
     var scrollToTopButton = document.getElementById('scrollToTopButton');
     if (window.scrollY > 100) {
         scrollToTopButton.style.visibility = 'visible';
     } else {
         scrollToTopButton.style.visibility = 'hidden';
     }
+}
+
+function clearScrollBtn(){
+    window.removeEventListener('scroll', toggleScrollToTopButton);
 }
 
 function scrollUpSmooth(){
